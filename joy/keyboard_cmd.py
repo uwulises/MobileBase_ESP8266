b20a2839d1ea81324ea80137d9f2024c184617db
uwulises <ulises.campodonico@ug.uchile.cl>
2023-04-24 20:08:47 -0400

    add keyboard control

--- joy/keyboard_cmd.py
+++ joy/keyboard_cmd.py
@@ -1,46 +1,42 @@
 import requests
 from pynput.keyboard import Key, Listener
 
-x_last = 0
-y_last = 0
-x_axis = 0
-y_axis = 0
-
 # function to send joystick command based on pressed key
-def send_command(event):
-    if event.name in mapping:
-        axis, value = mapping[event.name]
-        if (axis == "x_axis"):
-            x_axis = value
-        elif(axis == "y_axis"):
-            y_axis = value
-    elif event.name == 'esc':
-        # exit program on Escape key press
-        exit(0)
+def send_command(key):
+    key= str(key)
+    axis, value = mapping[key]
+    if (axis == "x_axis"):
+        x_val = value
+        y_val = 0
+    elif(axis == "y_axis"):
+        y_val = value
+        x_val = 0
+    else:
+        pass
+    call_req(x_val, y_val)
 
 # define keyboard-to-joystick mapping (arrows and X/Y analog)
 mapping = {
-    'up': ("x_axis", 1),
-    'down': ("x_axis", -1),
-    'left': ("y_axis", 1),
-    'right': ("y_axis", -1)
+    'Key.up': ("y_axis", 1),
+    'Key.down': ("y_axis", -1),
+    'Key.left': ("x_axis", -1),
+    'Key.right': ("x_axis", 1)
 }
 
-# if (x_axis != x_last or y_axis != y_last):
-#         url = "http://192.168.4.1/joystick"
-#         params = {"x_axis": x_axis, "y_axis": y_axis}
-#         #response = requests.get(url, params = params, timeout = 5)
-#         print("X axis: {}, Y axis: {},last X: {}, last Y: {}".format(x_axis, y_axis, x_last, y_last))
-#         x_last = x_axis
-#         y_last = y_axis
+#Call request using direction from key
+def call_req(x_val=0, y_val=0):
+    url = "http://192.168.4.1/joystick"
+    params = {"x_axis": x_val, "y_axis": y_val}
+    response = requests.get(url, params = params, timeout = 5)
+    print("X axis: {}, Y axis: {}".format(x_val, y_val))
 
-
+#Release key
 def on_release(key):
-    #print('{0} release'.format(key))
+    call_req()
     if key == Key.esc:
         # Stop listener
-        print('{0} release'.format(key))
         return False
+        
     
 # Collect events until released
 with Listener(
